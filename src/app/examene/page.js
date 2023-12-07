de3d775038b92f641850884e76fc9b2f78784b13
ibsence <88239982+1bsence@users.noreply.github.com>
2023-12-07 19:51:18 +0200

    Update page.js

--- src/app/examene/page.js
+++ src/app/examene/page.js
@@ -3,22 +3,21 @@
 import { ExaCom } from "./ExaCom";
 
 const SDED = new Date(2024, 1, 30);
-const pclp2 = new Date(2023, 6, 29);
-const pclp2_test2 = new Date(2023, 6, 7);
-const arca = new Date(2023, 7, 3);
-const coomunicare = new Date(2023, 6,13);
-const engleza = new Date(2023, 5,16);
+const pclp3 = new Date(2024, 2, 13);
+const fiz2 = new Date(2024, 1, 27);
+const sup = new Date(2024, 2,5);
+const ts = new Date(2024, 2,9);
+const cel1 = new Date(2024, 2,16);
 export default function Examene() {
   return (
     <>
       <div className="flex justify-evenly items-center flex-wrap">
         <ExaCom examen={SDED} text="SDED" />
-        <ExaCom examen={pclp2} text="PCLP2" />
-
- <ExaCom examen={pclp2_test2} text="PCLP2 - Test" />
-        <ExaCom examen={arca} text="ARCA" />
-        <ExaCom examen={coomunicare} text="COM" />
-        <ExaCom examen={engleza} text="ENG" />
+        <ExaCom examen={ts} text="TS" />
+        <ExaCom examen={pclp3} text="PCLP3"/>
+        <ExaCom examen={fiz2} text="FIZ2" />
+        <ExaCom examen={sup} text="SuP" />
+        <ExaCom examen={cel1} text="CEL1" />
       </div>
     </>
   );
